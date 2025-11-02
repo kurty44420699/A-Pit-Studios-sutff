b8e0ab6aa60ce31df95ed6bb98a7b6f3b9fa90aa
kurty yapper <kurtcyrill.yap@gmail.com>
2025-11-03 02:55:46 +0800

    Main menu and Pause Menu

---
+++ Main Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game Scene/game.tscn")
+
+
+func _on_options_pressed() -> void:
+	print("Nigga")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
---
+++ pause_menu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+func _ready():
+	visible = false  # hide on start
+
+func pause():
+	get_tree().paused = true
+	visible = true   # show menu when paused
+
+func resume():
+	get_tree().paused = false
+	visible = false  # hide menu when resumed
+
+func _on_resume_pressed():
+	resume()
+
+func _on_restart_pressed():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("Escape"):
+		if not get_tree().paused:
+			pause()
+		else:
+			resume()
