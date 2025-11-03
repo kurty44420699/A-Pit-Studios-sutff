5339987d216cde441ed07e3ab0416dafbc011182
kurty yapper <kurtcyrill.yap@gmail.com>
2025-11-03 21:17:37 +0800

    Added Dash for the player

--- Main Menu.gd
+++ Main Menu.gd
@@ -5,7 +5,7 @@ func _on_start_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	print("Nigga")
+	print("Options button pressed!")
 
 
 func _on_quit_pressed() -> void:
--- Scripts/Main Character Body2D.gd
+++ Scripts/Main Character Body2D.gd
@@ -2,25 +2,48 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -350.0
+const DASH_SPEED = 1000
+
+var dashing = false
+var can_dash = true
 
 @onready var sprite := $"AnimatedSprite2D"
 
 func _physics_process(delta: float) -> void:
 	# Gravity
-	velocity += get_gravity() * delta
-
+	if not is_on_floor(): # checks if mid air
+		velocity += get_gravity() * delta
+	
 	# Jump
-	if Input.is_action_just_pressed("Move_Up") and is_on_floor():
+	if Input.is_action_just_pressed("Move_Up") and is_on_floor(): #checks if it jumped
 		velocity.y = JUMP_VELOCITY
-
-	# Horizontal movement,Animation and flipping
-	var x_position = Input.get_axis("Move_Left", "Move_Right")
-	if x_position: 
-		velocity.x = SPEED * x_position
+	
+	# Dash checker
+	if Input.is_action_just_pressed("Dash") and can_dash: #checjs if it used dash button and hasn't dashed yet
+		dashing = true
+		can_dash = false
+		$Dash_timer.start()
+		$Can_dash_again_timer.start()
+	
+	# Horizontal movement, Dashing, Animation and Character flipping
+	var x_direction = Input.get_axis("Move_Left", "Move_Right")
+	if x_direction:
+		if dashing:
+			velocity.x = x_direction * DASH_SPEED
+		else:
+			velocity.x = x_direction * SPEED
 		sprite.play("Walking Animation")
-		sprite.flip_h = x_position < 0
+		sprite.flip_h = x_direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		sprite.play("Idle Animation")
-
+	
 	move_and_slide()
+
+func _on_dash_timer_timeout() -> void: 
+	dashing = false
+# this function makes it stop having super speed
+
+func _on_can_dash_again_timer_timeout() -> void:
+	if is_on_floor():
+		can_dash = true
