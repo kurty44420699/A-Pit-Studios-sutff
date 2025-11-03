4ce15d998f844b6ea1742f0d83798a22ac095d54
kurty yapper <kurtcyrill.yap@gmail.com>
2025-11-03 21:24:38 +0800

    Deleting useless lines

--- Main Menu.gd
+++ Main Menu.gd
@@ -3,10 +3,8 @@ extends Control
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Game Scene/game.tscn")
 
-
 func _on_options_pressed() -> void:
 	print("Options button pressed!")
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
--- pause_menu.gd
+++ pause_menu.gd
@@ -18,7 +18,6 @@ func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
-
 func _on_quit_pressed():
 	get_tree().quit()
 
